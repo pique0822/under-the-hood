a0dc18b8fbb3e8bdc3941c8a72a6df4b10e0941f
Jon Gauthier <jon@gauthiers.net>
2019-06-05 13:58:19 -0400

    clean up surgical_pipeline.sh

--- surgical_pipeline.sh
+++ surgical_pipeline.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
+
+SINGULARITY_IMG=/om2/user/jgauthie/singularity_images/deepo-cpu.simg
+LM_PATH=/om/group/cpl/language-models/colorlessgreenRNNs/hidden650_batch128_dropout0.2_lr20.0.pt
+PROJECT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" >/dev/null 2>&1 && pwd -P )"
+
+source /etc/profile.d/modules.sh
 module add openmind/singularity/3.0.3
 
-file_identifier = "TEST"
+file_identifier="TEST"
 # 1. Find most important coefficients
-cd garden_path/
-
+cd garden_path
 echo Finding important coefficients
-singularity exec -B /om/group/cpl -B /net/storage001.ib.cluster/om2/user/drmiguel/representational_analysis/under-the-hood /om2/user/jgauthie/singularity_images/deepo-cpu.simg python3 avg_surprisal_vbd_decoder.py --model_path /om/group/cpl/language-models/colorlessgreenRNNs/hidden650_batch128_dropout0.2_lr20.0.pt --training_cells reduced --cross_validation 10 --file_identifier $file_identifier
+singularity exec -B $PROJECT_PATH $SINGULARITY_IMG ls ../data
+singularity exec -B /om/group/cpl -B "$PROJECT_PATH" -B "$PROJECT_PATH/data" "$SINGULARITY_IMG" python3 avg_suprisal_vbd_decoder.py \
+	--model_path "$LM_PATH" --training_cells reduced --cross_validation 10 --file_identifier $file_identifier \
+	|| exit 1
 
 
 cd ..
@@ -17,9 +25,15 @@ for hidden_value in 0.1 1 10
 do
 
 	echo Current Hidden Value:: $hidden_value
-	singularity exec -B /om/group/cpl -B /net/storage001.ib.cluster/om2/user/drmiguel/representational_analysis/under-the-hood /om2/user/jgauthie/singularity_images/deepo-cpu.simg python3 evaluate_target_word_test_lower_surprisal.py --data ../data/colorlessgreenRNNs --checkpoint /om/group/cpl/language-models/colorlessgreenRNNs/hidden650_batch128_dropout0.2_lr20.0.pt --prefixfile prefixes.txt --surprisalmode True --outf surgical_gradient_r2_decrease_${file_identifier}_${hidden_value}.txt --modify_cell True --surgical_difference $hidden_value --file_identifier $file_identifier --gradient_type weight
+	singularity exec -B /om/group/cpl -B "$PROJECT_PATH" "$SINGULARITY_IMG" python3 evaluate_target_word_test_lower_surprisal.py \
+		--data ../data/colorlessgreenRNNs --checkpoint "$LM_PATH" \
+		--prefixfile prefixes.txt --surprisalmode True --outf surgical_gradient_r2_decrease_${file_identifier}_${hidden_value}.txt \
+		--modify_cell True --surgical_difference $hidden_value --file_identifier $file_identifier --gradient_type weight \
+		|| exit 2
 done
 
 # 3. Plot the results
 
-python3 generate_surprisal_plots_vbd.py --decrease_file_base surgical_gradient_r2_decrease_${file_identifier}_ --decrease_file_unique_ids 0.1 1 10 --surgical_decrease True --file_title Testing_Mechanism
+python3 generate_surprisal_plots_vbd.py --decrease_file_base surgical_gradient_r2_decrease_${file_identifier}_ \
+	--decrease_file_unique_ids 0.1 1 10 --surgical_decrease True --file_title Testing_Mechanism \
+	|| exit 3
